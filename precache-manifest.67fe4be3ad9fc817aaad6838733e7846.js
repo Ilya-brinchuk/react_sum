f1ba60b1d00b508b5001f30326a9909e6d9424bb
brynchuk <illia.brynchuk@nixsolutions.com>
2021-07-07 17:49:31 +0300

    Updates

---
+++ precache-manifest.67fe4be3ad9fc817aaad6838733e7846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33bbd1e6c1211941a25823dc0bec5d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be45e6e39e6026ea19a6",
+    "url": "./static/css/main.dea3523a.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be45e6e39e6026ea19a6",
+    "url": "./static/js/main.1b244470.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
